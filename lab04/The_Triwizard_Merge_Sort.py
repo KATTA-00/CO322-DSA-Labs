4b8f9fa3f9580780023b8f37aa0873f0a6e95dff
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-12-07 21:56:48 +0530

    Refactor merge_sort function and add swap function

--- lab04/The_Triwizard_Merge_Sort.py
+++ lab04/The_Triwizard_Merge_Sort.py
@@ -1,26 +1,51 @@
 t = int(input())
 
-def mergerSort(arr, start, end):
+def swap(arr):
+    if arr[0] > arr[1]:
+        temp = arr[0]
+        arr[0] = arr[1]
+        arr[1] = temp
 
-    mid = (start + end)//2
-    print(arr[start:end], start, end, mid)
+def combine(arr, start, mid, end):
+    arr_left = arr[start:mid]
+    arr_right = arr[mid:end+1]
 
-    if start < end and end-mid >= 2:
-        mergerSort(arr, mid+1, end)
+    if len(arr_left) == 2:
+        swap(arr_left)
+    if len(arr_right) == 2:
+        swap(arr_right)
 
-    if start < end and mid-start >= 2:
-        mergerSort(arr, start, mid)
+    arr_new = []
+    i = 0
+    j = 0
 
-    
+    while i < len(arr_left) and j < len(arr_right):
+        if arr_left[i] < arr_right[j]:
+            arr_new.append(arr_left[i])
+            i += 1
+        else:
+            arr_new.append(arr_right[j])
+            j += 1
+
+    arr_new.extend(arr_left[i:])
+    arr_new.extend(arr_right[j:])
+
+    for i in range(len(arr_new)):
+        arr[start + i] = arr_new[i]
+
+
+def merge_sort(arr, start, end):
+
+    if start < end:
+        mid = (start + end) // 2
+
+        merge_sort(arr, start, mid)
+        merge_sort(arr, mid+1, end)
+        combine(arr, start, mid+1, end)
 
 for _ in range(t):
     arr = list(map(int, input().strip().split()))
 
-    mergerSort(arr, 0, len(arr)-1)
-    print()
-
-
-    # print(" ".join([str(i) for i in arr]))
-
-
-
+    merge_sort(arr, 0, len(arr))
+    
+    print(" ".join(map(str, arr)))
